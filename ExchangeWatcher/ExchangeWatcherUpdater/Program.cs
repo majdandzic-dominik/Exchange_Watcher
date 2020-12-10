1628102bfbe5d3d5c37dc836f3c4deaede46f9b3
DocMinike <dominik.majdandzic@gmail.com>
2020-12-10 18:18:22 +0100

    Changed connection for ExchangeWatcherUpdater from local database to online database

---
+++ ExchangeWatcher/ExchangeWatcherUpdater/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+using MongoDB.Driver;
+
+
+namespace ExchangeWatcherUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            WebClient webClient = new WebClient();
+            String exchangeRatesJSON = webClient.DownloadString("http://api.hnb.hr/tecajn/v2");
+
+            dynamic dynamicExchangeRates = JsonConvert.DeserializeObject<dynamic>(exchangeRatesJSON);
+
+            
+            
+            
+            List<ExchangeRate> ExchangeRatesList = new List<ExchangeRate>();
+
+            foreach(dynamic dynamicObject in dynamicExchangeRates)
+            {
+                ExchangeRatesList.Add(
+                    new ExchangeRate(
+                        dynamicObject["datum_primjene"].ToString(),
+                        dynamicObject["drzava"].ToString(),
+                        dynamicObject["valuta"].ToString(),
+                        dynamicObject["jedinica"].ToString(),
+                        dynamicObject["srednji_tecaj"].ToString()
+                        )
+                );
+            }
+
+            string textToShow = "";
+
+            foreach(ExchangeRate exchangeRate in ExchangeRatesList)
+            {
+                textToShow += exchangeRate.GetAllValuesString();
+                textToShow += "\r\n";
+            }
+
+            Console.WriteLine(textToShow);
+
+
+            IMongoClient dbClient = new MongoClient("mongodb+srv://docMinike:5zPjAOGRX1Dwu3XK@exchangeratescluster.h4mjo.mongodb.net/docMinike?retryWrites=true&w=majority");
+            IMongoDatabase exchangeRatesDB = dbClient.GetDatabase("TestDB3");
+            var objectListCollection = exchangeRatesDB.GetCollection<ExchangeRate>("ExchangeRatesList");
+            objectListCollection.InsertMany(ExchangeRatesList);
+
+                       
+        }
+
+    }
+}
