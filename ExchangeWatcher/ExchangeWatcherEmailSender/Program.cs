bbf45cb97872d5b6d2526d5c172912f17612c98e
DocMinike <dominik.majdandzic@gmail.com>
2021-01-06 18:09:25 +0100

    Uncommented line of code

--- ExchangeWatcher/ExchangeWatcherEmailSender/Program.cs
+++ ExchangeWatcher/ExchangeWatcherEmailSender/Program.cs
@@ -62,7 +62,7 @@ namespace ExchangeWatcherEmailSender
                     }                    
                 }
                 //Console.WriteLine(emailSender.gettext());
-                //emailSender.SendEmail();
+                emailSender.SendEmail();
                 emailSender.ClearWatchedCurrencies();
             }
         }
